ManickaP/runtime

commit 6df065d5668e74dbc52427e36713a72677d62092

[browser] Bundler friendly build output (#116300)

src/tasks/Microsoft.NET.Sdk.WebAssembly.Pack.Tasks/BootJsonBuilderHelper.cs
@@ -3,6 +3,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -18,6 +19,7 @@ namespace Microsoft.NET.Sdk.WebAssembly
     {
 #pragma warning disable SYSLIB1045 // Convert to 'GeneratedRegexAttribute'.
         internal static readonly Regex mergeWithPlaceholderRegex = new Regex(@"/\*!\s*dotnetBootConfig\s*\*/\s*{}");
+        internal static readonly Regex bundlerFriendlyImportsRegex = new Regex(@"/\*!\s*bundlerFriendlyImports\s*\*/");
 #pragma warning restore SYSLIB1045 // Convert to 'GeneratedRegexAttribute'.
 
         private static readonly string[] coreAssemblyNames = [
@@ -52,18 +54,33 @@ namespace Microsoft.NET.Sdk.WebAssembly
             return false;
         }
 
-        public void WriteConfigToFile(BootJsonData config, string outputPath, string? outputFileExtension = null, string? mergeWith = null)
+        public void WriteConfigToFile(BootJsonData config, string outputPath, string? outputFileExtension = null, string? mergeWith = null, string? imports = null)
         {
             var output = JsonSerializer.Serialize(config, JsonOptions);
+
+            // Remove the $#[ and ]#$" that are used to mark JS variable usage.
+            output = output
+                .Replace("\"$#[", string.Empty)
+                .Replace("]#$\"", string.Empty);
 
             outputFileExtension ??= Path.GetExtension(outputPath);
             Log.LogMessage($"Write config in format '{outputFileExtension}'");
             if (mergeWith != null)
             {
                 string existingContent = File.ReadAllText(mergeWith);
-                output = mergeWithPlaceholderRegex.Replace(existingContent, e => $"/*json-start*/{output}/*json-end*/");
-                if (existingContent.Equals(output))
-                    Log.LogError($"Merging boot config into '{mergeWith}' failed to find the placeholder.");
+                output = ReplaceWithAssert(
+                    mergeWithPlaceholderRegex,
+                    existingContent,
+                    $"/*json-start*/{output}/*json-end*/",
+                    $"Merging boot config into '{mergeWith}' failed to find the placeholder."
+                );
+
+                output = ReplaceWithAssert(
+                    bundlerFriendlyImportsRegex,
+                    output,
+                    imports ?? string.Empty,
+                    $"Failed to find the placeholder for bundler friendly imports."
+                );
             }
             else if (outputFileExtension == ".js")
             {
@@ -73,8 +90,20 @@ namespace Microsoft.NET.Sdk.WebAssembly
             File.WriteAllText(outputPath, output);
         }
 
+        private string ReplaceWithAssert(Regex regex, string content, string replacement, string errorMessage)
+        {
+            string existingContent = content;
+            content = regex.Replace(content, e => replacement);
+            if (existingContent.Equals(content))
+                Log.LogError(errorMessage);
+
+            return content;
+        }
+
         public void ComputeResourcesHash(BootJsonData bootConfig)
         {
+            ResourcesData resources = (ResourcesData)bootConfig.resources;
+
             var sb = new StringBuilder();
 
             static void AddDictionary(StringBuilder sb, Dictionary<string, string>? res)
@@ -86,59 +115,61 @@ namespace Microsoft.NET.Sdk.WebAssembly
                     sb.Append(assetHash);
             }
 
-            AddDictionary(sb, bootConfig.resources.assembly);
-            AddDictionary(sb, bootConfig.resources.coreAssembly);
+            AddDictionary(sb, resources.assembly);
+            AddDictionary(sb, resources.coreAssembly);
 
-            AddDictionary(sb, bootConfig.resources.jsModuleWorker);
-            AddDictionary(sb, bootConfig.resources.jsModuleDiagnostics);
-            AddDictionary(sb, bootConfig.resources.jsModuleNative);
-            AddDictionary(sb, bootConfig.resources.jsModuleRuntime);
-            AddDictionary(sb, bootConfig.resources.wasmNative);
-            AddDictionary(sb, bootConfig.resources.wasmSymbols);
-            AddDictionary(sb, bootConfig.resources.icu);
-            AddDictionary(sb, bootConfig.resources.runtime);
-            AddDictionary(sb, bootConfig.resources.lazyAssembly);
+            AddDictionary(sb, resources.jsModuleWorker);
+            AddDictionary(sb, resources.jsModuleDiagnostics);
+            AddDictionary(sb, resources.jsModuleNative);
+            AddDictionary(sb, resources.jsModuleRuntime);
+            AddDictionary(sb, resources.wasmNative);
+            AddDictionary(sb, resources.wasmSymbols);
+            AddDictionary(sb, resources.icu);
+            AddDictionary(sb, resources.runtime);
+            AddDictionary(sb, resources.lazyAssembly);
 
-            if (bootConfig.resources.satelliteResources != null)
+            if (resources.satelliteResources != null)
             {
-                foreach (var culture in bootConfig.resources.satelliteResources)
+                foreach (var culture in resources.satelliteResources)
                     AddDictionary(sb, culture.Value);
             }
 
-            if (bootConfig.resources.vfs != null)
+            if (resources.vfs != null)
             {
-                foreach (var entry in bootConfig.resources.vfs)
+                foreach (var entry in resources.vfs)
                     AddDictionary(sb, entry.Value);
             }
 
-            if (bootConfig.resources.coreVfs != null)
+            if (resources.coreVfs != null)
             {
-                foreach (var entry in bootConfig.resources.coreVfs)
+                foreach (var entry in resources.coreVfs)
                     AddDictionary(sb, entry.Value);
             }
 
-            bootConfig.resources.hash = Utils.ComputeTextIntegrity(sb.ToString());
+            resources.hash = Utils.ComputeTextIntegrity(sb.ToString());
         }
 
         public Dictionary<string, string>? GetNativeResourceTargetInBootConfig(BootJsonData bootConfig, string resourceName)
         {
+            ResourcesData resources = (ResourcesData)bootConfig.resources;
+
             string resourceExtension = Path.GetExtension(resourceName);
             if (resourceName.StartsWith("dotnet.native.worker", StringComparison.OrdinalIgnoreCase) && string.Equals(resourceExtension, ".mjs", StringComparison.OrdinalIgnoreCase))
-                return bootConfig.resources.jsModuleWorker ??= new();
+                return resources.jsModuleWorker ??= new();
             else if (resourceName.StartsWith("dotnet.diagnostics", StringComparison.OrdinalIgnoreCase) && string.Equals(resourceExtension, ".js", StringComparison.OrdinalIgnoreCase))
-                return bootConfig.resources.jsModuleDiagnostics ??= new();
+                return resources.jsModuleDiagnostics ??= new();
             else if (resourceName.StartsWith("dotnet.native", StringComparison.OrdinalIgnoreCase) && string.Equals(resourceExtension, ".js", StringComparison.OrdinalIgnoreCase))
-                return bootConfig.resources.jsModuleNative ??= new();
+                return resources.jsModuleNative ??= new();
             else if (resourceName.StartsWith("dotnet.runtime", StringComparison.OrdinalIgnoreCase) && string.Equals(resourceExtension, ".js", StringComparison.OrdinalIgnoreCase))
-                return bootConfig.resources.jsModuleRuntime ??= new();
+                return resources.jsModuleRuntime ??= new();
             else if (resourceName.StartsWith("dotnet.native", StringComparison.OrdinalIgnoreCase) && string.Equals(resourceExtension, ".wasm", StringComparison.OrdinalIgnoreCase))
-                return bootConfig.resources.wasmNative ??= new();
+                return resources.wasmNative ??= new();
             else if (resourceName.StartsWith("dotnet", StringComparison.OrdinalIgnoreCase) && string.Equals(resourceExtension, ".js", StringComparison.OrdinalIgnoreCase))
                 return null;
             else if (resourceName.StartsWith("dotnet.native", StringComparison.OrdinalIgnoreCase) && string.Equals(resourceExtension, ".symbols", StringComparison.OrdinalIgnoreCase))
-                return bootConfig.resources.wasmSymbols ??= new();
+                return resources.wasmSymbols ??= new();
             else if (resourceName.StartsWith("icudt", StringComparison.OrdinalIgnoreCase))
-                return bootConfig.resources.icu ??= new();
+                return resources.icu ??= new();
             else
                 Log.LogError($"The resource '{resourceName}' is not recognized as any native asset");
 
@@ -173,6 +204,140 @@ namespace Microsoft.NET.Sdk.WebAssembly
                 return null;
 
             return intValue;
+        }
+
+        public string TransformResourcesToAssets(BootJsonData config, bool bundlerFriendly = false)
+        {
+            List<string> imports = [];
+
+            ResourcesData resources = (ResourcesData)config.resources;
+            var assets = new AssetsData();
+
+            assets.hash = resources.hash;
+            assets.jsModuleRuntime = MapJsAssets(resources.jsModuleRuntime);
+            assets.jsModuleNative = MapJsAssets(resources.jsModuleNative);
+            assets.jsModuleWorker = MapJsAssets(resources.jsModuleWorker);
+            assets.jsModuleDiagnostics = MapJsAssets(resources.jsModuleDiagnostics);
+
+            assets.wasmNative = resources.wasmNative?.Select(a =>
+            {
+                var asset = new WasmAsset()
+                {
+                    name = a.Key,
+                    integrity = a.Value
+                };
+
+                if (bundlerFriendly)
+                {
+                    string escaped = EscapeName(a.Key);
+                    imports.Add($"import {escaped} from \"./{a.Key}\";");
+                    asset.resolvedUrl = EncodeJavascriptVariableInJson(escaped);
+                }
+
+                return asset;
+            }).ToList();
+            assets.wasmSymbols = resources.wasmSymbols?.Select(a => new SymbolsAsset()
+            {
+                name = a.Key,
+            }).ToList();
+
+            assets.icu = MapGeneralAssets(resources.icu);
+            assets.coreAssembly = MapGeneralAssets(resources.coreAssembly);
+            assets.assembly = MapGeneralAssets(resources.assembly);
+            assets.corePdb = MapGeneralAssets(resources.corePdb);
+            assets.pdb = MapGeneralAssets(resources.pdb);
+            assets.lazyAssembly = MapGeneralAssets(resources.lazyAssembly);
+
+            if (resources.satelliteResources != null)
+            {
+                assets.satelliteResources = resources.satelliteResources.ToDictionary(
+                    kvp => kvp.Key,
+                    kvp => MapGeneralAssets(kvp.Value, variableNamePrefix: kvp.Key, subFolder: kvp.Key)
+                );
+            }
+
+            assets.libraryInitializers = resources.libraryInitializers;
+            assets.modulesAfterConfigLoaded = MapJsAssets(resources.modulesAfterConfigLoaded);
+            assets.modulesAfterRuntimeReady = MapJsAssets(resources.modulesAfterRuntimeReady);
+
+            assets.extensions = resources.extensions;
+
+            assets.coreVfs = MapVfsAssets(resources.coreVfs);
+            assets.vfs = MapVfsAssets(resources.vfs);
+
+            if (bundlerFriendly && config.appsettings != null)
+            {
+                config.appsettings = config.appsettings.Select(a =>
+                {
+                    string escaped = EscapeName(a);
+                    imports.Add($"import {escaped} from \"./{a}\";");
+                    return EncodeJavascriptVariableInJson(escaped);
+                }).ToList();
+            }
+
+            string EscapeName(string name) => Utils.FixupSymbolName(name);
+            string EncodeJavascriptVariableInJson(string name) => $"$#[{name}]#$";
+
+            List<GeneralAsset>? MapGeneralAssets(Dictionary<string, string>? assets, string? variableNamePrefix = null, string? subFolder = null) => assets?.Select(a =>
+            {
+                var asset = new GeneralAsset()
+                {
+                    virtualPath = resources.fingerprinting?[a.Key] ?? a.Key,
+                    name = a.Key,
+                    integrity = a.Value
+                };
+
+                if (bundlerFriendly)
+                {
+                    string escaped = EscapeName(string.Concat(subFolder, a.Key));
+                    string subFolderWithSeparator = subFolder != null ? $"{subFolder}/" : string.Empty;
+                    imports.Add($"import {escaped} from \"./{subFolderWithSeparator}{a.Key}\";");
+                    asset.resolvedUrl = EncodeJavascriptVariableInJson(escaped);
+                }
+
+                return asset;
+            }).ToList();
+
+            List<JsAsset>? MapJsAssets(Dictionary<string, string>? assets, string? variableNamePrefix = null, string? subFolder = null) => assets?.Select(a =>
+            {
+                var asset = new JsAsset()
+                {
+                    name = a.Key
+                };
+
+                if (bundlerFriendly)
+                {
+                    string escaped = EscapeName(string.Concat(subFolder, a.Key));
+                    string subFolderWithSeparator = subFolder != null ? $"{subFolder}/" : string.Empty;
+                    imports.Add($"import * as {escaped} from \"./{subFolderWithSeparator}{a.Key}\";");
+                    asset.moduleExports = EncodeJavascriptVariableInJson(escaped);
+                }
+
+                return asset;
+            }).ToList();
+
+            List<VfsAsset>? MapVfsAssets(Dictionary<string, Dictionary<string, string>>? assets) => assets?.Select(a =>
+            {
+                var asset = new VfsAsset()
+                {
+                    virtualPath = a.Key,
+                    name = a.Value.Keys.First(),
+                    integrity = a.Value.Values.First()
+                };
+
+                if (bundlerFriendly)
+                {
+                    string escaped = EscapeName(string.Concat(asset.name));
+                    imports.Add($"import * as {escaped} from \"./{asset.name}\";");
+                    asset.resolvedUrl = EncodeJavascriptVariableInJson(escaped);
+                }
+
+                return asset;
+            }).ToList();
+
+            config.resources = assets;
+
+            return string.Join(Environment.NewLine, imports);
         }
     }
 }

src/tasks/Microsoft.NET.Sdk.WebAssembly.Pack.Tasks/BootJsonData.cs
@@ -28,6 +28,9 @@ public class BootJsonData
     public string applicationEnvironment { get; set; }
 
     /// <summary>
+    /// For .NET < 10, this contains <see cref="ResourcesData"/>.
+    /// For .NET >= 10, this contains <see cref="AssetsData"/>.
+    /// ---
     /// Gets the set of resources needed to boot the application. This includes the transitive
     /// closure of .NET assemblies (including the entrypoint assembly), the dotnet.wasm file,
     /// and any PDBs to be loaded.
@@ -36,7 +39,8 @@ public class BootJsonData
     /// and values are SHA-256 hashes formatted in prefixed base-64 style (e.g., 'sha256-abcdefg...')
     /// as used for subresource integrity checking.
     /// </summary>
-    public ResourcesData resources { get; set; } = new ResourcesData();
+    [DataMember(EmitDefaultValue = false)]
+    public object resources { get; set; } = new ResourcesData();
 
     /// <summary>
     /// Gets a value that determines whether to enable caching of the <see cref="resources"/>
@@ -247,6 +251,133 @@ public class ResourcesData
 
     [DataMember(EmitDefaultValue = false)]
     public List<string> remoteSources { get; set; }
+}
+
+public class AssetsData
+{
+    /// <summary>
+    /// Gets a hash of all resources
+    /// </summary>
+    public string hash { get; set; }
+
+    [DataMember(EmitDefaultValue = false)]
+    public List<JsAsset> jsModuleWorker { get; set; }
+
+    [DataMember(EmitDefaultValue = false)]
+    public List<JsAsset> jsModuleDiagnostics { get; set; }
+
+    [DataMember(EmitDefaultValue = false)]
+    public List<JsAsset> jsModuleNative { get; set; }
+
+    [DataMember(EmitDefaultValue = false)]
+    public List<JsAsset> jsModuleRuntime { get; set; }
+
+    [DataMember(EmitDefaultValue = false)]
+    public List<WasmAsset> wasmNative { get; set; }
+
+    [DataMember(EmitDefaultValue = false)]
+    public List<SymbolsAsset> wasmSymbols { get; set; }
+
+    [DataMember(EmitDefaultValue = false)]
+    public List<GeneralAsset> icu { get; set; }
+
+    /// <summary>
+    /// "assembly" (.dll) resources needed to start MonoVM
+    /// </summary>
+    public List<GeneralAsset> coreAssembly { get; set; } = new();
+
+    /// <summary>
+    /// "assembly" (.dll) resources
+    /// </summary>
+    public List<GeneralAsset> assembly { get; set; } = new();
+
+    /// <summary>
+    /// "debug" (.pdb) resources needed to start MonoVM
+    /// </summary>
+    [DataMember(EmitDefaultValue = false)]
+    public List<GeneralAsset> corePdb { get; set; }
+
+    /// <summary>
+    /// "debug" (.pdb) resources
+    /// </summary>
+    [DataMember(EmitDefaultValue = false)]
+    public List<GeneralAsset> pdb { get; set; }
+
+    /// <summary>
+    /// localization (.satellite resx) resources
+    /// </summary>
+    [DataMember(EmitDefaultValue = false)]
+    public Dictionary<string, List<GeneralAsset>> satelliteResources { get; set; }
+
+    /// <summary>
+    /// Assembly (.dll) resources that are loaded lazily during runtime
+    /// </summary>
+    [DataMember(EmitDefaultValue = false)]
+    public List<GeneralAsset> lazyAssembly { get; set; }
+
+    /// <summary>
+    /// JavaScript module initializers that Blazor will be in charge of loading.
+    /// </summary>
+    [DataMember(EmitDefaultValue = false)]
+    public ResourceHashesByNameDictionary libraryInitializers { get; set; }
+
+    [DataMember(EmitDefaultValue = false)]
+    public List<JsAsset> modulesAfterConfigLoaded { get; set; }
+
+    [DataMember(EmitDefaultValue = false)]
+    public List<JsAsset> modulesAfterRuntimeReady { get; set; }
+
+    /// <summary>
+    /// Extensions created by users customizing the initialization process. The format of the file(s)
+    /// is up to the user.
+    /// </summary>
+    [DataMember(EmitDefaultValue = false)]
+    public Dictionary<string, ResourceHashesByNameDictionary> extensions { get; set; }
+
+    [DataMember(EmitDefaultValue = false)]
+    public List<VfsAsset> coreVfs { get; set; }
+
+    [DataMember(EmitDefaultValue = false)]
+    public List<VfsAsset> vfs { get; set; }
+}
+
+[DataContract]
+public class JsAsset
+{
+    public string name { get; set; }
+    public string moduleExports { get; set; }
+}
+
+[DataContract]
+public class SymbolsAsset
+{
+    public string name { get; set; }
+}
+
+[DataContract]
+public class WasmAsset
+{
+    public string name { get; set; }
+    public string integrity { get; set; }
+    public string resolvedUrl { get; set; }
+}
+
+[DataContract]
+public class GeneralAsset
+{
+    public string virtualPath { get; set; }
+    public string name { get; set; }
+    public string integrity { get; set; }
+    public string resolvedUrl { get; set; }
+}
+
+[DataContract]
+public class VfsAsset
+{
+    public string virtualPath { get; set; }
+    public string name { get; set; }
+    public string integrity { get; set; }
+    public string resolvedUrl { get; set; }
 }
 
 public enum GlobalizationMode : int

src/mono/wasm/Wasm.Build.Tests/ModuleConfigTests.cs
@@ -57,7 +57,7 @@ public class ModuleConfigTests : WasmTemplateTestsBase
         );
     }
 
-    [Fact]
+    [Fact, TestCategory("bundler-friendly")]
     public async Task OutErrOverrideWorks()
     {
         Configuration config = Configuration.Debug;

src/mono/sample/wasm/browser-advanced/main.js
@@ -39,9 +39,9 @@ try {
         .withConfig({
             maxParallelDownloads: 1,
             resources: {
-                modulesAfterConfigLoaded: {
-                    "advanced-sample.lib.module.js": ""
-                }
+                modulesAfterConfigLoaded: [{
+                    "name": "advanced-sample.lib.module.js"
+                }]
             }
         })
         .withModuleConfig({

src/libraries/sendtohelix-browser.targets
@@ -138,6 +138,7 @@
     <_XUnitTraitArg Condition="'$(TestUsingWorkloads)' == 'true'">-notrait category=no-workload</_XUnitTraitArg>
     <_XUnitTraitArg Condition="'$(TestUsingWorkloads)' != 'true'">-trait category=no-workload</_XUnitTraitArg>
     <_XUnitTraitArg Condition="'$(WasmFingerprintAssets)' == 'false'">$(_XUnitTraitArg) -trait category=no-fingerprinting</_XUnitTraitArg>
+    <_XUnitTraitArg Condition="'$(WasmBundlerFriendlyBootConfig)' == 'true'">$(_XUnitTraitArg) -trait category=bundler-friendly</_XUnitTraitArg>
   </PropertyGroup>
 
   <PropertyGroup>

src/libraries/sendtohelix-wasm.targets
@@ -14,6 +14,7 @@
     <WorkItemPrefix Condition="'$(TestUsingWorkloads)' != 'true'">NoWorkload-</WorkItemPrefix>
     <WorkItemPrefix Condition="'$(WasmEnableWebcil)' == 'false'">NoWebcil-</WorkItemPrefix>
     <WorkItemPrefix Condition="'$(WasmFingerprintAssets)' == 'false'">NoFingerprint-</WorkItemPrefix>
+    <WorkItemPrefix Condition="'$(WasmBundlerFriendlyBootConfig)' == 'true'">JavascriptBundler-</WorkItemPrefix>
     <WorkItemPrefix Condition="'$(WasmEnableThreads)' != 'true'">$(WorkItemPrefix)ST-</WorkItemPrefix>
     <WorkItemPrefix Condition="'$(WasmEnableThreads)' == 'true'">$(WorkItemPrefix)MT-</WorkItemPrefix>
   </PropertyGroup>
@@ -50,7 +51,7 @@
 
     <!-- for testing with workloads, we use separate items -->
     <ItemGroup>
-      <HelixWorkItem Include="@(BuildWasmApps_PerJobList->'$(WorkItemPrefix)%(Identity)')" Condition="'$(TestUsingWorkloads)' == 'true' and '$(WasmFingerprintAssets)' == 'true'">
+      <HelixWorkItem Include="@(BuildWasmApps_PerJobList->'$(WorkItemPrefix)%(Identity)')" Condition="'$(TestUsingWorkloads)' == 'true' and '$(WasmFingerprintAssets)' == 'true' and '$(WasmBundlerFriendlyBootConfig)' == 'false'">
         <PayloadArchive>$(_BuildWasmAppsPayloadArchive)</PayloadArchive>
         <PreCommands Condition="'$(OS)' == 'Windows_NT'">set &quot;HELIX_XUNIT_ARGS=-class %(Identity)&quot;</PreCommands>
         <PreCommands Condition="'$(OS)' != 'Windows_NT'">export &quot;HELIX_XUNIT_ARGS=-class %(Identity)&quot;</PreCommands>
@@ -58,7 +59,7 @@
         <Timeout>$(_workItemTimeout)</Timeout>
       </HelixWorkItem>
 
-      <HelixWorkItem Include="$(WorkItemPrefix)Wasm.Build.Tests" Condition="'$(TestUsingWorkloads)' != 'true' or '$(WasmFingerprintAssets)' != 'true'">
+      <HelixWorkItem Include="$(WorkItemPrefix)Wasm.Build.Tests" Condition="'$(TestUsingWorkloads)' != 'true' or '$(WasmFingerprintAssets)' != 'true' or '$(WasmBundlerFriendlyBootConfig)' != 'false'">
         <PayloadArchive>$(_BuildWasmAppsPayloadArchive)</PayloadArchive>
         <Command>$(HelixCommand)</Command>
         <Timeout>$(_workItemTimeout)</Timeout>

commit a63ff10fd069601def7e11383f187f9c2ee09181

[release/10.0] [browser] Generate boot config with hash field instead of integrity (#122391)
Backport of https://github.com/dotnet/runtime/pull/122353 to
release/10.0 to fix #121876

/cc @maraf

## Customer Impact

- [x] Customer reported
- [x] Found internally

Requests to WebAssembly resources were missing integrity attribute,
which is in our case used as a safety mechanism against stale cache.
Since by default all assets are fingerprinted, we do not rely on the
integrity validation, but we still have it in the public API for asset
loading.

## Regression

- [x] Yes
- [ ] No

Caused by https://github.com/dotnet/runtime/pull/116300

## Testing

Automated and manual test.

## Risk

Low. The change renames generated field from unexpected value to a
correct one. Since the content is embedded in the file from runtime
pack, there is no room for inconsistency due to stale cache.

Co-authored-by: Pavel Savara <pavel.savara@gmail.com>
Co-authored-by: Larry Ewing <lewing@microsoft.com>

src/mono/wasm/testassets/WasmBasicTestApp/App/wwwroot/main.js
@@ -95,6 +95,12 @@ switch (testCase) {
             }
         });
         break;
+    case "AssetIntegrity":
+        dotnet.withResourceLoader((type, name, defaultUri, integrity, behavior) => {
+            testOutput(`Asset '${name}' has integrity '${integrity}'`);
+            return defaultUri;
+        });
+        break;
     case "OutErrOverrideWorks":
         dotnet.withModuleConfig({
             out: (message) => {
@@ -232,6 +238,7 @@ try {
             exit(0);
             break;
         case "DownloadResourceProgressTest":
+        case "AssetIntegrity":
             exit(0);
             break;
         case "OutErrOverrideWorks":

src/tasks/Microsoft.NET.Sdk.WebAssembly.Pack.Tasks/BootJsonBuilderHelper.cs
@@ -224,7 +224,7 @@ namespace Microsoft.NET.Sdk.WebAssembly
                 var asset = new WasmAsset()
                 {
                     name = a.Key,
-                    integrity = a.Value,
+                    hash = a.Value,
                     cache = GetCacheControl(a.Key, resources)
                 };
 
@@ -286,7 +286,7 @@ namespace Microsoft.NET.Sdk.WebAssembly
                 {
                     virtualPath = resources.fingerprinting?[a.Key] ?? a.Key,
                     name = a.Key,
-                    integrity = a.Value,
+                    hash = a.Value,
                     cache = GetCacheControl(a.Key, resources)
                 };
 
@@ -326,7 +326,7 @@ namespace Microsoft.NET.Sdk.WebAssembly
                 {
                     virtualPath = a.Key,
                     name = assetName,
-                    integrity = a.Value.Values.First(),
+                    hash = a.Value.Values.First(),
                     cache = GetCacheControl(assetName, resources)
                 };
 

src/tasks/Microsoft.NET.Sdk.WebAssembly.Pack.Tasks/BootJsonData.cs
@@ -358,7 +358,7 @@ public class SymbolsAsset
 public class WasmAsset
 {
     public string name { get; set; }
-    public string integrity { get; set; }
+    public string hash { get; set; }
     public string resolvedUrl { get; set; }
     public string cache { get; set; }
 }
@@ -368,7 +368,7 @@ public class GeneralAsset
 {
     public string virtualPath { get; set; }
     public string name { get; set; }
-    public string integrity { get; set; }
+    public string hash { get; set; }
     public string resolvedUrl { get; set; }
     public string cache { get; set; }
 }
@@ -378,7 +378,7 @@ public class VfsAsset
 {
     public string virtualPath { get; set; }
     public string name { get; set; }
-    public string integrity { get; set; }
+    public string hash { get; set; }
     public string resolvedUrl { get; set; }
     public string cache { get; set; }
 }

src/mono/wasm/Wasm.Build.Tests/ModuleConfigTests.cs
@@ -105,4 +105,21 @@ public class ModuleConfigTests : WasmTemplateTestsBase
             m => Assert.Equal("Managed code has run", m)
         );
     }
+
+    [Fact, TestCategory("bundler-friendly")]
+    public async Task AssetIntegrity()
+    {
+        Configuration config = Configuration.Debug;
+        ProjectInfo info = CopyTestAsset(config, false, TestAsset.WasmBasicTestApp, $"AssetIntegrity");
+        PublishProject(info, config);
+
+        var result = await RunForPublishWithWebServer(new BrowserRunOptions(
+            Configuration: config,
+            TestScenario: "AssetIntegrity"
+        ));
+        Assert.False(
+            result.TestOutput.Any(m => !m.Contains(".js") && !m.Contains(".json") && m.Contains("has integrity ''")),
+            "There are assets without integrity hash"
+        );
+    }
 }